4aaecb88719da721cbd60e02a6de683b81b74a62
rbarne17 <rbarne17@rbarne17-laptop.uthsc.edu>
2017-01-18 17:23:20 -0600

    converted to IterativeRobot

--- MechWarrior-2017/src/org/usfirst/frc/team4524/robot/Robot.java
+++ MechWarrior-2017/src/org/usfirst/frc/team4524/robot/Robot.java
@@ -1,8 +1,5 @@
 package org.usfirst.frc.team4524.robot;
 
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.SampleRobot;
-
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
@@ -12,47 +9,74 @@ import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * This is a demo program showing the use of the RobotDrive class. The
- * SampleRobot class is the base of a robot application that will automatically
- * call your Autonomous and OperatorControl methods at the right time as
- * controlled by the switches on the driver station or the field controls.
- *
  * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the SampleRobot
+ * functions corresponding to each mode, as described in the IterativeRobot
  * documentation. If you change the name of this class or the package after
  * creating this project, you must also update the manifest file in the resource
  * directory.
- *
- * WARNING: While it may look like a good choice to use for your code if you're
- * inexperienced, don't. Unless you know what you are doing, complex code will
- * be much more difficult under this system. Use IterativeRobot or Command-Based
- * instead if you're new.
  */
-public class Robot extends SampleRobot {
-	RobotDrive myRobot = new RobotDrive(0, 1);
-	Joystick stick = new Joystick(0);
+public class Robot extends IterativeRobot {
 	final String defaultAuto = "Default";
 	final String customAuto = "My Auto";
+	String autoSelected;
 	SendableChooser<String> chooser = new SendableChooser<>();
+	RobotDrive myRobot = new RobotDrive(0, 1);
+	Joystick stick = new Joystick(0);
+	Timer timer = new Timer();
 
-	public Robot() {
-		myRobot.setExpiration(0.1);
-	}
 
-	public void robotInit2() {
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
 		chooser.addDefault("Default Auto", defaultAuto);
 		chooser.addObject("My Auto", customAuto);
-		SmartDashboard.putData("Auto modes", chooser);
-		System.out.println("This is the official code for MechWarriors-2017");
-		System.out.println("Initializing...");
+		SmartDashboard.putData("Auto choices", chooser);
+		Thread visionThread = new Thread(() -> {
+			// Get the UsbCamera from CameraServer
+			UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+			// Set the resolution
+			camera.setResolution(640, 480);
 
+			// Get a CvSink. This will capture Mats from the camera
+			CvSink cvSink = CameraServer.getInstance().getVideo();
+			// Setup a CvSource. This will send images back to the Dashboard
+			CvSource outputStream = CameraServer.getInstance().putVideo("Rectangle", 640, 480);
 
+			// Mats are very memory expensive. Lets reuse this Mat.
+			Mat mat = new Mat();
+
+			// This cannot be 'true'. The program will never exit if it is. This
+			// lets the robot stop this thread when restarting robot code or
+			// deploying.
+			while (!Thread.interrupted()) {
+				// Tell the CvSink to grab a frame from the camera and put it
+				// in the source mat.  If there is an error notify the output.
+				if (cvSink.grabFrame(mat) == 0) {
+					// Send the output the error.
+					outputStream.notifyError(cvSink.getError());
+					// skip the rest of the current iteration
+					continue;
+				}
+				// Put a rectangle on the image
+				Imgproc.rectangle(mat, new Point(100, 100), new Point(400, 400),
+						new Scalar(255, 255, 255), 5);
+				// Give the output stream a new image to display
+				outputStream.putFrame(mat);
+			}
+		});
+		visionThread.setDaemon(true);
+		visionThread.start();
 	}
 
 	/**
@@ -63,23 +87,28 @@ public class Robot extends SampleRobot {
 	 * getString line to get the auto name from the text box below the Gyro
 	 *
 	 * You can add additional auto modes by adding additional comparisons to the
-	 * if-else structure below with additional strings. If using the
+	 * switch structure below with additional strings. If using the
 	 * SendableChooser make sure to add them to the chooser code above as well.
 	 */
 	@Override
-	public void autonomous() {
-		String autoSelected = chooser.getSelected();
-		// String autoSelected = SmartDashboard.getString("Auto Selector",
+	public void autonomousInit() {
+		autoSelected = chooser.getSelected();
+		// autoSelected = SmartDashboard.getString("Auto Selector",
 		// defaultAuto);
 		System.out.println("Auto selected: " + autoSelected);
+	}
 
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	@Override
+	public void autonomousPeriodic() {
 		switch (autoSelected) {
 		case customAuto:
 			myRobot.setSafetyEnabled(false);
 			myRobot.drive(-0.5, 1.0); // spin at half speed
 			Timer.delay(2.0); // for 2 seconds
-			myRobot.drive(0.0, 0.0); // stop robot
-			break;
+			myRobot.drive(0.0, 0.0); // stop robot			break;
 		case defaultAuto:
 		default:
 			myRobot.setSafetyEnabled(false);
@@ -91,61 +120,18 @@ public class Robot extends SampleRobot {
 	}
 
 	/**
-	 * Runs the motors with arcade steering.
+	 * This function is called periodically during operator control
 	 */
 	@Override
-	public void operatorControl() {
-		myRobot.setSafetyEnabled(true);
-		while (isOperatorControl() && isEnabled()) {
-			myRobot.arcadeDrive(stick); // drive with arcade style (use right
-										// stick)
-			Timer.delay(0.005); // wait for a motor update time
-		}
-	}
+	public void teleopPeriodic() {
+		myRobot.arcadeDrive(stick);
+}
 
 	/**
-	 * Runs during test mode
+	 * This function is called periodically during test mode
 	 */
 	@Override
-	public void test() {
+	public void testPeriodic() {
 	}
-
-
-public void robotInit() {
-	Thread visionThread = new Thread(() -> {
-		// Get the UsbCamera from CameraServer
-		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-		// Set the resolution
-		camera.setResolution(640, 480);
-
-		// Get a CvSink. This will capture Mats from the camera
-		CvSink cvSink = CameraServer.getInstance().getVideo();
-		// Setup a CvSource. This will send images back to the Dashboard
-		CvSource outputStream = CameraServer.getInstance().putVideo("Rectangle", 640, 480);
-
-		// Mats are very memory expensive. Lets reuse this Mat.
-		Mat mat = new Mat();
-
-		// This cannot be 'true'. The program will never exit if it is. This
-		// lets the robot stop this thread when restarting robot code or
-		// deploying.
-		while (!Thread.interrupted()) {
-			// Tell the CvSink to grab a frame from the camera and put it
-			// in the source mat.  If there is an error notify the output.
-			if (cvSink.grabFrame(mat) == 0) {
-				// Send the output the error.
-				outputStream.notifyError(cvSink.getError());
-				// skip the rest of the current iteration
-				continue;
-			}
-			// Put a rectangle on the image
-			Imgproc.rectangle(mat, new Point(100, 100), new Point(400, 400),
-					new Scalar(255, 255, 255), 5);
-			// Give the output stream a new image to display
-			outputStream.putFrame(mat);
-		}
-	});
-	visionThread.setDaemon(true);
-	visionThread.start();
 }
-}
+
