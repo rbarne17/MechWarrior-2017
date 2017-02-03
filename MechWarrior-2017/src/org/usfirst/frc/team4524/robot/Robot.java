bb9ec190acc90e487a1ea2f00295ae5cb2090949
firstrobotics <firstrobotics@firstrobotics-Latitude-E5550>
2017-02-02 18:11:45 -0600

    most recent.

--- MechWarrior-2017/src/org/usfirst/frc/team4524/robot/Robot.java
+++ MechWarrior-2017/src/org/usfirst/frc/team4524/robot/Robot.java
@@ -17,7 +17,6 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
