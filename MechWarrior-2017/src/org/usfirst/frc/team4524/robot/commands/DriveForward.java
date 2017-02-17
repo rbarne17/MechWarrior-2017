de8bc3a602ec1610127cf9b1caa6ee0d5040a950
rbarne17 <rbarne17@rbarne17-laptop.uthsc.edu>
2017-02-16 18:26:39 -0600

    working on distance based on time.

--- MechWarrior-2017/src/org/usfirst/frc/team4524/robot/commands/DriveForward.java
+++ MechWarrior-2017/src/org/usfirst/frc/team4524/robot/commands/DriveForward.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team4524.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 import org.usfirst.frc.team4524.robot.Robot;
@@ -11,6 +12,8 @@ import org.usfirst.frc.team4524.robot.Robot;
 public class DriveForward extends Command {
 	private double driveForwardSpeed;
 	private double distance;
+	private final double fps = 9.3888; //full speed
+	private Timer timer = new Timer();
 	private double error;
 	private final double kTolerance = 0.1;
 	private final double kP = -1.0 / 5.0;
@@ -20,7 +23,7 @@ public class DriveForward extends Command {
 	}
 
 	public DriveForward(double dist) {
-		this(dist, 0.5);
+		this(dist, -0.5);
 	}
 
 	public DriveForward(double dist, double maxSpeed) {
@@ -32,22 +35,26 @@ public class DriveForward extends Command {
 	@Override
 	protected void initialize() {
 		Robot.driveTrain.reset();
-		setTimeout(10);
+		timer.reset();
+		timer.start();
+		setTimeout(2.0);
 	}
 
 	@Override
 	protected void execute() {
-		error = (distance - Robot.driveTrain.getDistance());
-		if (driveForwardSpeed * kP * error >= driveForwardSpeed) {
-			Robot.driveTrain.drive(driveForwardSpeed, driveForwardSpeed);
-		} else {
-			Robot.driveTrain.drive(driveForwardSpeed * kP * error, driveForwardSpeed * kP * error);
-		}
+		System.out.println(Math.abs(((timer.get() / fps) * driveForwardSpeed)));
+		Robot.driveTrain.drive(driveForwardSpeed,driveForwardSpeed);
+//		error = (distance - Robot.driveTrain.getDistance());
+//		if (driveForwardSpeed * kP * error >= driveForwardSpeed) {
+//			Robot.driveTrain.drive(driveForwardSpeed, driveForwardSpeed);
+//		} else {
+//			Robot.driveTrain.drive(driveForwardSpeed * kP * error, driveForwardSpeed * kP * error);
+//		}
 	}
 
 	@Override
 	protected boolean isFinished() {
-		return (Math.abs(error) <= kTolerance) || isTimedOut();
+		return (Math.abs(((timer.get() * fps) / driveForwardSpeed)) >= distance);
 	}
 
 	@Override
